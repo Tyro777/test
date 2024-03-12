d7fe76908e9624fbf1b3a0e48bfa15a770aad715
Tyro777 <1796761485@qq.com>
2024-03-12 16:27:28 +0800

    java2

---
+++ test2.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+
+int main() {
+
+    int i = 0;
+    printf("加入比特\n");
+    while (i<= 20000) {
+        printf("敲第%d行代码,\n",i);
+        i++;
+       
+    }
+    if (i >= 20000)
+        printf("拿一个好offer\n");
+    return 0;
+}
